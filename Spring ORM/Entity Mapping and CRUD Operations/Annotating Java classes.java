5995a7731706cf3f279597452c8b935f679b10e6
VenkataSairaja <137163264+VenkataSairaja@users.noreply.github.com>
2024-06-14 12:22:34 +0530

    Add files via upload

---
+++ Spring ORM/Entity Mapping and CRUD Operations/Annotating Java classes.java
@@ -0,0 +1,15 @@
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "salary")
+    private double salary;
+
+    // Getters and setters
+}
